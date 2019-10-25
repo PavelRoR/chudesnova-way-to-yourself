cca34e7d7b9a531838b9097428ef7610792c52d8
pavelp21 <pavelvp2182@yandex.ru>
2019-10-25 23:17:03 +0500

    Page is stylezed

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -34,10 +34,10 @@ $(document).ready(function () {
                     showPage = function (i) {
                         $(tabs).children("div").children("div").hide();
                         $(tabs).children("div").children("div").eq(i).show();
-                        $(tabs).children("ul").children("li").removeClass("active-");
+                        $(tabs).children("ul").children("li").removeClass("active");
                         $(tabs).children("ul").children("li").eq(i).addClass("active");
                     };
-                showPage(1);
+                showPage(0);
                 $(tabs).children("ul").children("li").each(function (index, element) {
                     $(element).attr("data-page", i);
                     i++;
